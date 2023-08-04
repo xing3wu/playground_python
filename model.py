3ffe01302fd3e66c29fc270f24d648df377c27fd
git.xing3.wu <git.xing3.wu@outlook.com>
2023-08-04 23:54:20 +0000

    marshmallow

---
+++ model.py
@@ -0,0 +1,37 @@
+import datetime as dt
+from marshmallow import Schema, fields, post_load
+from pprint import pprint
+
+
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.created_at = dt.datetime.now()
+
+    def __repr__(self):
+        return "<User(name={self.name!r})>".format(self=self)
+
+
+class UserSchema(Schema):
+    name = fields.Str()
+    email = fields.Email()
+    created_at = fields.DateTime()
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        return User(**data)
+
+
+if __name__ == "__main__":
+    user_data = {
+        "created_at": "2014-08-11T05:26:03.869245",
+        "email": "ken@yahoo.com",
+        "name": "Ken",
+    }
+    schema = UserSchema(only=("name", "email"))
+    result = schema.load(user_data)
+
+    print(type(result))
+    pprint(result)
+
